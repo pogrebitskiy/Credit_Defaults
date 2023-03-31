ad71b7ad0ebc7f623d6e8da3d7f614c230f1d1ee
Jacob Kulik <jakul238@gmail.com>
2023-03-30 22:08:17 -0400

    knn update

--- knn.py
+++ knn.py
@@ -3,31 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import random as rnd
 import numpy as np
+from utilities import utilities as util
 
-
-def nfolds(df, n_folds):
-    '''To split the data into a given number of folds'''
-    # Find the size of each fold
-    fold_size = len(df) // n_folds
-    df = df.sample(frac=1).reset_index(drop=True)
-    folds = []
-
-    # Create each fold
-    for i in range(n_folds):
-        start = i * fold_size
-        end = (i + 1) * fold_size
-        if i == n_folds - 1:
-            end = len(df)
-        folds.append(df[start:end])
-
-    return folds
-
-def knn(train_set, test_set, k):
+def knn(train_set, test_set, k=10):
     '''To perform K-Nearest Neighbors on the train and test sets'''
     predictions = []
-    for test_row in test_set:
+    for test_row in test_set.values:
         distances = []
-        for train_row in train_set:
+        for train_row in train_set.values:
             dist = np.sqrt(np.sum((test_row[:-1] - train_row[:-1])**2))
             distances.append((dist, train_row[-1]))
         distances.sort()
@@ -35,10 +18,19 @@ def knn(train_set, test_set, k):
         output = [row[-1] for row in neighbors]
         prediction = max(set(output), key=output.count)
         predictions.append(prediction)
-    return predictions
+
+    metrics = util.metrics(test_set.iloc[:, -1].to_list(), predictions)
+
+    print(metrics)
+
+    return metrics
 
 if __name__ == '__main__':
     df = pd.read_csv('balanced_credit.csv')
 
-    folds = nfolds(df, 10)
+    folds = util.nfolds(df, 10)
+
+    result = util.cross_validate(folds, knn)
+
+    print(result)
 
